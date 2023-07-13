639f794d0af577ab45976314bac21c11ae8d4bd1
soheil <soheilghazimoradi@gmail.com>
2023-07-13 06:56:55 +0330

    complete player texts

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int winingsOfXPlayer = 0;
+  int winingsOfOPlayer = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -18,7 +20,49 @@ class _HomePageState extends State<HomePage> {
           title: const Text("Tic Tac Toe"),
         ),
         body: Column(
-          children: [],
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "Player O",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      winingsOfOPlayer.toString(),
+                      style: const TextStyle(fontSize: 25),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "Player X",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      winingsOfXPlayer.toString(),
+                      style: const TextStyle(fontSize: 25),
+                    )
+                  ],
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
